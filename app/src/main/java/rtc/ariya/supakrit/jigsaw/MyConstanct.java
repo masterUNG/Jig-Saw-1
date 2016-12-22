4befe10ff65938365abc83117eee07876ce4b522
masterUNG <phrombutr@gmail.com>
2016-12-23 01:37:33 +0700

    finish 23dec

--- app/src/main/java/rtc/ariya/supakrit/jigsaw/MyConstanct.java
+++ app/src/main/java/rtc/ariya/supakrit/jigsaw/MyConstanct.java
@@ -10,6 +10,19 @@ public class MyConstanct {
             R.drawable.a3, R.drawable.a4, R.drawable.a5, R.drawable.a6,
             R.drawable.a7, R.drawable.a8, R.drawable.a9, R.drawable.a10};
 
+    private int[] a1_3x3Ints = new int[]{R.drawable.a1_01,
+            R.drawable.a1_02, R.drawable.a1_03, R.drawable.a1_04, R.drawable.a1_05,
+            R.drawable.a1_06, R.drawable.a1_07, R.drawable.a1_08, R.drawable.a1_09};
+    private int[] ran_a1_3x3Ints = new int[]{1,0,7,5,4,6,3,8,2};
+
+    public int[] getA1_3x3Ints() {
+        return a1_3x3Ints;
+    }
+
+    public int[] getRan_a1_3x3Ints() {
+        return ran_a1_3x3Ints;
+    }
+
     public int[] getImageInt() {
         return imageInt;
     }
--- app/src/main/java/rtc/ariya/supakrit/jigsaw/Play3x3.java
+++ app/src/main/java/rtc/ariya/supakrit/jigsaw/Play3x3.java
@@ -1,13 +1,16 @@
 package rtc.ariya.supakrit.jigsaw;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
 
 public class Play3x3 extends AppCompatActivity {
 
     //Explicit
     private int indexAnInt, imageAnInt;
+    private MyConstanct myConstanct;
+    private ImageView[] imageClickImageViews;
 
 
     @Override
@@ -15,12 +18,45 @@ public class Play3x3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play3x3);
 
+        bindWidget();
+
+        //Setting
+        myConstanct = new MyConstanct();
         indexAnInt = getIntent().getIntExtra("Index", 0);
         imageAnInt = getIntent().getIntExtra("Image", R.drawable.a1);
         Log.d("19decV3", "index ==>" + indexAnInt);
         Log.d("19decV3", "Image ==>  " + imageAnInt);
 
+        createView();
+
 
     }   // Main Method
+
+    private void createView() {
+
+        int[] indexRandom = myConstanct.getRan_a1_3x3Ints();
+        int[] a1_3x3Ints = myConstanct.getA1_3x3Ints();
+
+        for (int i=0;i<indexRandom.length;i++) {
+            imageClickImageViews[indexRandom[i]].setImageResource(a1_3x3Ints[i]);
+        }
+
+    }
+
+    private void bindWidget() {
+
+        imageClickImageViews = new ImageView[9];
+        imageClickImageViews[0] = (ImageView) findViewById(R.id.imageView17);
+        imageClickImageViews[1] = (ImageView) findViewById(R.id.imageView18);
+        imageClickImageViews[2] = (ImageView) findViewById(R.id.imageView19);
+        imageClickImageViews[3] = (ImageView) findViewById(R.id.imageView20);
+        imageClickImageViews[4] = (ImageView) findViewById(R.id.imageView21);
+        imageClickImageViews[5] = (ImageView) findViewById(R.id.imageView22);
+        imageClickImageViews[6] = (ImageView) findViewById(R.id.imageView23);
+        imageClickImageViews[7] = (ImageView) findViewById(R.id.imageView24);
+        imageClickImageViews[8] = (ImageView) findViewById(R.id.imageView25);
+
+
+    }
 
 }   // Main Class
